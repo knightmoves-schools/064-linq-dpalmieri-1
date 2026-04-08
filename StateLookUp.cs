02a11687245c930c3dbc0a41d96f6a4684b70433
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-04-08 19:03:42 +0000

    seeing if line was unnecessary

--- StateLookUp.cs
+++ StateLookUp.cs
@@ -3,12 +3,11 @@ using static StateList;
 public class StateLookUp{
 
     public IEnumerable<string> LookUp(string search){
-        var matches = new List<string>();
 
-        var LINQmatches = from state in STATES 
+        var matches = from state in STATES 
                           where state.StartsWith(search) 
                           orderby state ascending
                           select state;
-        return LINQmatches;
+        return matches;
     }
 }
